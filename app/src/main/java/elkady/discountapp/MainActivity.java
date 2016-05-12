3761278c302325272f8e7df7657d11df7ed841e2
sahar55555 <sahar1elkady@hotmail.com>
2016-05-12 16:20:29 -0600

    Added some onCreate activities or initial commit, we don't know.

--- app/src/main/java/elkady/discountapp/MainActivity.java
+++ app/src/main/java/elkady/discountapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener
 {
@@ -13,16 +14,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // small change for git
     }
 
     @Override
     public void onClick(View v) {
-        String store_id;
+        String store_id = "not initialized";
         switch(v.getId()) {
             case R.id.albertsons_button:
                 store_id = "albertsons";
                 break;
         }
+        Toast.makeText(this, store_id, Toast.LENGTH_LONG).show();
         //Intent i = new Intent(fragmentname.class);
         // somewhere in here we pass the store_id argument
         // startActivity(i);
