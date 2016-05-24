2a2b7f0954559eaaa235eb46618ea2c426a72cac
David King <davidcking.inbox@gmail.com>
2016-05-24 00:47:39 -0600

    Added handling of the optional detail field in Sales constructor and toString. Minor changes to front screen.

--- app/src/main/java/elkady/discountapp/Sales.java
+++ app/src/main/java/elkady/discountapp/Sales.java
@@ -1,6 +1,5 @@
 package elkady.discountapp;
 
-import android.os.Debug;
 import android.util.Log;
 
 /**
@@ -10,68 +9,86 @@ public class Sales
 {
     private String ProductName;
     public enum Stores {
-        KING_SOOPERS(1), SPROUTS(2), SAFE_WAY(3), ALBERTSONS(4);
+        KWIK_E_MART(0), KING_SOOPERS(1), SPROUTS(2), SAFE_WAY(3), ALBERTSONS(4);
         private final int index;
         Stores(int i) {
             this.index = i;
         }
-    };
+    }
     public enum Category {
-        Nothing,
-        Beverage,
-        Bakery,
-        Cleaning,
-        Dairy,
-        MeatSeafood,
-        Produce
-    };
+        NOTHING,
+        BEVERAGE,
+        BAKERY,
+        CLEANING,
+        DAIRY,
+        MEAT_SEAFOOD,
+        PRODUCE
+    }
 
-    public static Category DeptFromString(String sVal) {
+    public static Category DepthFromString(String sVal) {
         if (sVal.compareToIgnoreCase("beverage") == 0) {
-            return Category.Beverage;
+            return Category.BEVERAGE;
         }
         if (sVal.compareToIgnoreCase("meat") == 0) {
-            return Category.MeatSeafood;
+            return Category.MEAT_SEAFOOD;
         }
         if (sVal.compareToIgnoreCase("seafood") == 0) {
-            return Category.MeatSeafood;
+            return Category.MEAT_SEAFOOD;
         }
         if (sVal.compareToIgnoreCase("meatandseafood") == 0) {
-            return Category.MeatSeafood;
+            return Category.MEAT_SEAFOOD;
         }
         if (sVal.compareToIgnoreCase("bakery") == 0) {
-            return Category.Bakery;
+            return Category.BAKERY;
         }
-        if (sVal.compareToIgnoreCase("Dairy") == 0) {
-            return Category.Dairy;
+        if (sVal.compareToIgnoreCase("DAIRY") == 0) {
+            return Category.DAIRY;
         }
         if (sVal.compareToIgnoreCase("fruit") == 0) {
-            return Category.Produce;
+            return Category.PRODUCE;
         }
         if (sVal.compareToIgnoreCase("vegetables") == 0) {
-            return Category.Produce;
+            return Category.PRODUCE;
         }
         if (sVal.compareToIgnoreCase("produce") == 0) {
-            return Category.Produce;
+            return Category.PRODUCE;
+        }
+        if (sVal.compareToIgnoreCase("home") == 0) {
+            return Category.CLEANING;
         }
         if (sVal.compareToIgnoreCase("laundry") == 0) {
-            return Category.Cleaning;
+            return Category.CLEANING;
         }
-        return Category.Nothing;
+        return Category.NOTHING;
     }
 
     private Stores Shop;
     private Category Dept;
     private float Price;
+    private String detail;
 
     public Sales(String prName,float prc, Stores store, Category cat)
     {
-        this.setProductName(prName);
-        this.setShop(store);
-        this.setPrice(prc);
-        this.setDept(cat);
+        this(prName, prc, store, cat, "");
     }
 
+    public Sales(String prName,float prc, Stores store, Category cat, String det)
+    {
+        setProductName(prName);
+        setShop(store);
+        setPrice(prc);
+        setDept(cat);
+        setDetail(det);
+    }
+    public boolean hasDetailField() {
+        return detail.length() > 0;
+    }
+    public void setDetail(String d) {
+        this.detail = d;
+    }
+    public String getDetail() {
+        return this.detail;
+    }
     public void setProductName(String proName)
     {
         this.ProductName=proName;
@@ -83,7 +100,7 @@ public class Sales
 
     public void setShop(Stores shp)
     {
-        this.Shop=shp;
+        this.Shop = shp;
     }
     public Stores getShop()
     {
@@ -92,7 +109,7 @@ public class Sales
 
     public void setPrice(float prc)
     {
-        this.Price=prc;
+        this.Price = prc;
     }
     public float getPrice()
     {
@@ -101,7 +118,7 @@ public class Sales
 
     public void setDept(Category cat)
     {
-        this.Dept =cat;
+        this.Dept = cat;
     }
     public Category getDept()
     {
@@ -111,7 +128,31 @@ public class Sales
     @Override
     public String toString()
     {
-        return getShop() + ","+ getProductName() + "; " + String.format("$%.2f per unit.", getPrice());
+        if (hasDetailField()) {
+            return AbbrevStringFromStores(getShop()) + "," + getProductName() + "[" + getDetail() + "]; " +
+                    String.format("$%.2f.", getPrice()) +
+                    " " + getDept();
+        }
+        else
+        {
+            return AbbrevStringFromStores(getShop()) + "," + getProductName() + "; " + String.format("$%.2f.", getPrice()) +
+                " " + getDept();
+        }
+
+    }
+
+    public static String AbbrevStringFromStores(Stores store) {
+        switch(store) {
+            case KING_SOOPERS:
+                return "KS";
+            case ALBERTSONS:
+                return "Alb";
+            case SAFE_WAY:
+                return "SW";
+            case SPROUTS:
+                return "SP";
+        }
+        return "??";
     }
 
     public static Stores storeValueFromString(String sval)
@@ -124,7 +165,7 @@ public class Sales
             return Stores.SAFE_WAY;
         if (sval.compareToIgnoreCase("SP") == 0)
             return Stores.SPROUTS;
-        return Stores.KING_SOOPERS;
+        return Stores.KWIK_E_MART;
     }
 
     public static Sales parseFromString(String s) {
@@ -136,9 +177,9 @@ public class Sales
         String productName = fields[1];
         String productDetail = fields[2];
         float productPrice = Float.valueOf(fields[3]);
-        String productDept = fields[3];
+        String productDept = fields[4];
 
-        return new Sales(productName, productPrice, store, DeptFromString(productDept));
+        return new Sales(productName, productPrice, store, DepthFromString(productDept), productDetail);
     }
 
 
